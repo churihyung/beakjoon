03372d97455ff4f185961f1442d995479eb27252
churihyung <dlwk0807@gmail.com>
2022-04-01 21:58:09 +0900

    그리디문제
    끝나는 시간만 정렬해서 한번 틀렸음
    1.끝나는 시간으로 정렬하고 같은시간일 경우는 2.시작시간으로도 정렬해야한다.

---
+++ stepby/1931.py
@@ -0,0 +1,25 @@
+n = int(input())
+a=[]
+for _ in range(n):
+    a.append(list(map(int,input().split())))
+
+a.sort(key = lambda x :(x[1],x[0]))
+
+
+e_time=0
+cnt =0
+
+while True:
+    for i,x in enumerate(a):
+        if i ==0:
+            cnt+=1
+            e_time=x[1]
+        else:
+            if x[0]>=e_time:
+                cnt+=1
+                e_time=x[1]
+            
+        
+    break
+
+print(cnt)
